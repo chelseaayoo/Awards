9e15d1c89ba8ec2e5a13881cd8516605afe2806d
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-12-13 03:49:47 -0500

    create home view function

---
+++ awardz/views.py
@@ -0,0 +1,18 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .forms import Post_projectform,ReveiwForm,UpdateProfile,UserUpdateform
+from django.contrib import messages
+from .models import Project_Post,Profile,Reviews,Rates
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from .serialiers import ProfileSerializer,ProjectSerializer
+# from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    projects = Project_Post.get_all_projects()
+    return render(request,"home.html",{"projects":projects})
