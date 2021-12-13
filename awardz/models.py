22af82597c1e9f93bf09c256eea4c4546df72a9d
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-12-13 03:36:27 -0500

    add project_post model

--- awardz/models.py
+++ awardz/models.py
@@ -9,3 +9,23 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to="profile/",default = "default.jpg")
     bio = HTMLField()
     updated_on = models.DateTimeField(auto_now_add=True)
+
+class Project_Post(models.Model):
+    posted_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    img_project = models.ImageField(upload_to="project/")
+    project_url = models.URLField()
+    description = TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100,blank=True)
+    
+    
+    def get_all_projects():
+        projects = Project_Post.objects.all()
+        return projects
+    
+    @classmethod
+    def get_project_by_id(cls,id):
+        project = Project_Post.objects.filter(id=id)
+        
+        return project
+        
