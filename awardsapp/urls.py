631c1143e617bcc0921314d19be0a98b743ee56c
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-12-13 04:38:12 -0500

    add awardz url to url patterns

---
+++ awardsapp/urls.py
@@ -0,0 +1,28 @@
+"""awardsapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from awardz.views import  logout_view
+# from rest_framework import routers, serializers
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('awardz.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/',logout_view,name="logout"),
+]
